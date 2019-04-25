aec1c9c091f9edb0c077791201757848992d87cf
Marcin Z <mz2019.031@outlook.com>
2019-04-25 09:20:13 +0200

    Począte 2giego dnia

---
+++ TaskLib/ToDoItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szkolenie.Tasks
+{
+    public class ToDoItem
+    {
+        public static int LastID=0;
+        public int TaskID;
+        public string Title;
+        public bool Done;
+        public DateTime DueDate;
+
+        static ToDoItem()
+        {
+            LastID = 100;
+            Console.WriteLine("Static Constructor");
+        }
+
+
+        public ToDoItem()
+        {
+            Title = "Brak";
+            DueDate = DateTime.Now.AddDays(1);
+            TaskID = NewID();
+        }
+
+        public ToDoItem(string title):this()
+        {
+            Title = title;
+            DueDate = DateTime.Now.AddDays(1);
+            //TaskID = 1;
+        }
+
+        public ToDoItem(string title, DateTime date):this(title)
+        {
+            //Title = title;
+            DueDate = date;
+            //TaskID = 1;
+        }
+
+        public string Info()
+        {
+            return $"Task:{TaskID}, Title: {Title}, Date: {DueDate}, Done:{Done}";
+        }
+
+        public static int NewID()
+        {
+            return ++LastID;
+        }
+
+    }
+}
+
---
+++ TasksRuntime/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Szkolenie.Tasks;
+
+namespace TasksRuntime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDoItem td1 = new ToDoItem("Task 1")
+            {
+                DueDate = DateTime.Now.AddDays(3)
+            };
+
+            ToDoItem td2 = td1;
+            td2.Title = "Nowy tytul";
+
+            ToDoItem td3 = new ToDoItem("Task 3", DateTime.Now);
+
+            Console.WriteLine(td1.Info());
+            Console.WriteLine(td2.Info());
+            Console.WriteLine(td3.Info());
+
+
+        }
+    }
+}
