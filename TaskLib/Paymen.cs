6714288e82eec32ed57872d499fd85a113b008c9
Marcin Z <mz2019.031@outlook.com>
2019-04-25 14:22:04 +0200

    Paymen added

---
+++ TaskLib/Paymen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szkolenie.Tasks
+{
+    class Payment:ToDoItem
+    {
+        public DateTime? PaymentDate { get; set; }
+        public decimal Amount { get; private set; }
+        public decimal Balance { get; private set; }
+        
+        public Payment(string title, DateTime date, decimal amount )
+            : base(title, date)
+        {
+            Amount = amount;
+        }
+
+        public void Refund(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Interest()
+        {
+
+        }
+    }
+}
